9e9145893b111e272d6f42596ea6f2d395b91241
Boluwatiiife <tundelawalb@gmail.com>
2024-11-17 18:05:00 +0100

    finance logger

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplates.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+//inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  list.render(doc, type.value, "end");
+});
+
+// GENERICS
+
+const addUID = <T extends { name: string }>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let docOne = addUID({ name: "bolu", age: 23 });
+
+console.log(docOne.name);
+
+// with interfaces
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+// tuples
+
+let arr = ["red", 23, true];
+let tup: [string, number, boolean] = ["realll", 34, false];
+
+let student: [string, number];
+student = ["david", 23443];
